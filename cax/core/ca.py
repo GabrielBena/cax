09027bb0001d795b92274d11c549144e8819be5f
Gabriel Béna <gabriel.bena@spinncloud.com>
2024-10-11 18:48:59 +0100

    first mutli-task setup, kinda working with one_hot encodings

--- cax/core/ca.py
+++ cax/core/ca.py
@@ -3,44 +3,48 @@
 from functools import partial
 
 from flax import nnx
+import jax
+import jax.numpy as jnp
 
 from cax.core.perceive.perceive import Perceive
+from cax.core.state import State
 from cax.core.update.update import Update
-from cax.types import Input, State
+from cax.types import Input
+from cax.nn.vae import Encoder
 
 
 class CA(nnx.Module):
-	"""Cellular Automata class."""
+    """Cellular Automata class."""
 
-	perceive: Perceive
-	update: Update
+    perceive: Perceive
+    update: Update
 
-	def __init__(self, perceive: Perceive, update: Update):
-		"""Initialize the CA.
+    def __init__(self, perceive: Perceive, update: Update):
+        """Initialize the CA.
 
-		Args:
-			perceive: Perception module.
-			update: Update module.
+        Args:
+                perceive: Perception module.
+                update: Update module.
 
-		"""
-		self.perceive = perceive
-		self.update = update
+        """
+        self.perceive = perceive
+        self.update = update
 
 	@nnx.jit
 	def step(self, state: State, input: Input | None = None) -> State:
 		"""Perform a single step of the CA.
 
-		Args:
-			state: Current state.
-			input: Optional input.
+        Args:
+                state: Current state.
+                input: Optional input.
 
-		Returns:
-			Updated state.
+        Returns:
+                Updated state.
 
-		"""
-		perception = self.perceive(state)
-		state = self.update(state, perception, input)
-		return state
+        """
+        perception = self.perceive(state)
+        state = self.update(state, perception, input)
+        return state
 
 	@partial(nnx.jit, static_argnames=("num_steps", "all_steps", "input_in_axis"))
 	def __call__(
@@ -54,27 +58,41 @@ class CA(nnx.Module):
 	) -> State:
 		"""Run the CA for multiple steps.
 
-		Args:
-			state: Initial state.
-			input: Optional input.
-			num_steps: Number of steps to run.
-			all_steps: Whether to return all intermediate states.
-			input_in_axis: Axis for input if provided for each step.
+        Args:
+                state: Initial state.
+                input: Optional input.
+                num_steps: Number of steps to run.
+                all_steps: Whether to return all intermediate states.
+                input_in_axis: Axis for input if provided for each step.
 
-		Returns:
-			Final state or all intermediate states if all_steps is True.
+        Returns:
+                Final state or all intermediate states if all_steps is True.
 
-		"""
+        """
 
 		def step(carry: tuple[CA, State], input: Input | None) -> tuple[tuple[CA, State], State]:
 			ca, state = carry
 			state = ca.step(state, input)
 			return (ca, state), state if all_steps else None  # type: ignore
 
-		(_, state), states = nnx.scan(
-			step,
-			in_axes=(nnx.Carry, input_in_axis),
-			length=num_steps,
-		)((self, state), input)
+        (_, state), states = nnx.scan(
+            step,
+            in_axes=(nnx.Carry, input_in_axis),
+            length=num_steps,
+        )((self, state), input)
 
-		return states if all_steps else state
+        return states if all_steps else state
+
+
+class UnsupervisedCA(CA):
+    encoder: Encoder
+
+    def __init__(self, perceive, update, encoder):
+        super().__init__(perceive, update)
+
+        self.encoder = encoder
+
+    def encode(self, target, key):
+        mean, logvar = self.encoder(target)
+        target_enc = mean + jax.random.normal(key, mean.shape) * jnp.exp(0.5 * logvar)
+        return target_enc
